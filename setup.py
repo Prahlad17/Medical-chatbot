7a5149ee72e9a83c38a51d9458f3a081fb478f50
Prahlad17 <prahladpai40@gmail.com>
2024-09-15 13:00:19 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Medical Chatbot',
+    version='0.0.0',
+    author='Prahlad pai',
+    author_email='prahladpai40@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+)
